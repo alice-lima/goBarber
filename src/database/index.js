7231b51d3724c1aca7a68c07703c085007a560b6
Gabriel Sandrini <gabriel.sandrini@outlook.com>
2019-12-24 23:50:14 -0300

    upload do avatar do usuario

--- src/database/index.js
+++ src/database/index.js
@@ -2,8 +2,9 @@ import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
 import DatabaseConfig from '../config/database';
+import File from '../app/models/File';
 
-const models = [User];
+const models = [User, File];
 class Database {
   constructor() {
     this.init();
@@ -12,7 +13,9 @@ class Database {
   init() {
     this.connection = new Sequelize(DatabaseConfig);
 
-    models.map(model => model.init(this.connection));
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 
